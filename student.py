cf033507a88bdfc77615c78bee2bd9338e0f12b1
Primalpancake92 <johnle.jl39@gmail.com>
2024-11-13 10:53:36 +1100

    second commit for the student.py file.

--- student.py
+++ student.py
@@ -29,32 +29,68 @@ class Student:
     
     
     def get_name(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """
         formatted = self.first_name + " " + self.last_name
         return formatted
     
     
     def get_age(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """
         formatted = str(self.age)
         return formatted
     
     
     def get_grade(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """
         return self.grade
     
     
     def set_student_id(self, student_id: int):
+        """_summary_
+
+        Args:
+            student_id (int): _description_
+        """
         self.student_id = student_id
         
     
     def set_full_name(self, first_name: str, last_name: str):
+        """_summary_
+
+        Args:
+            first_name (str): _description_
+            last_name (str): _description_
+        """
         self.first_name = first_name
         self.last_name = last_name
         
     
     def set_age(self, age: int):
+        """_summary_
+
+        Args:
+            age (int): _description_
+        """
         self.age = age
     
     
     def set_grade(self, grade: str):
+        """_summary_
+
+        Args:
+            grade (str): _description_
+        """
         self.grade = grade
         
